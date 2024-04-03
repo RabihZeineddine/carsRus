62ea2147db7513ff870251022fd856b1e100e61c
RabihZeineddine <112440776+RabihZeineddine@users.noreply.github.com>
2024-04-03 14:22:27 -0700

    Update createAccountList.py

--- createAccountList.py
+++ createAccountList.py
@@ -1,1 +1,8 @@
+class Account:
+  def __init__(self, email, passWord, firstName, lastName):
+    self.email = email
+    self.passWord = passWord
+    self.firstName = firstName
+    self.lastName = lastName
+    
 
