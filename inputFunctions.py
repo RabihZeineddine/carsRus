104127287839f756894219f212893ae6892a1f6e
RabihZeineddine <112440776+RabihZeineddine@users.noreply.github.com>
2024-04-03 13:57:55 -0700

    Update inputFunctions.py

--- inputFunctions.py
+++ inputFunctions.py
@@ -1,1 +1,25 @@
+def inputEmail():
+    email = input("")
+    return email
+
+def inputPasscode():
+    passcode = input("")
+    return passcode
+
+def inputCC():
+    creditCard = input("")
+    return creditCard
+
+def inputAddress():
+    address = input("")
+    return address
+
+def inputFirstName():
+    firstName = input("")
+    return firstName
+
+def inputLastName():
+    lastName = input("")
+    return lastName
+
 
