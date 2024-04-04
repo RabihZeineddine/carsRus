e40aeb7b8fd26e160d29f3a975e43f77688147f8
just-maki <165954874+just-maki@users.noreply.github.com>
2024-04-04 13:17:04 -0700

    Update createCarList.py

--- createCarList.py
+++ createCarList.py
@@ -1,13 +1,44 @@
 class Cars:
-def __init__(vin, make, model, year, price, color, type, vin, milage = None):
-    vin.make = make
-    vin.model = model
-    vin.year = year
-    vin.price = price
-    vin.color = color
-    vin.type = type
-    vin.vin = vin
+def __init__(user, make, model, year, price, color, type, vin, favorites, milage = None):
+    user.make = make
+    user.model = model
+    user.year = year
+    user.price = price
+    user.color = color
+    user.type = type
+    user.vin = vin
+
+def set_make(car, make)
+    car.make = make
+
+def set_model(car, model)
+    car.model = model
+
+def set_year(car, year)
+    car.year = year
     
+def set_price(car, price)
+    car.price = price
+
+def set_color(car, color)
+    car.color = color
+
+def set_type(car, type)
+    car.type = type
+
+def set_vin(car, vin)
+    car.vin = vin
+
+def set_color(car, color)
+    car.color = color
+
+def add_to_fav(car)
+    car.favorites = true
+
+def remove_from_fav(car)
+    car.favorites = false
+
+
 car_list = []
 
 def add_user()
