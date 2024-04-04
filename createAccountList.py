8eb391efc37391ccc6258893ff2e97fdd108bbea
CrispyDAWG <132181160+CrispyDAWG@users.noreply.github.com>
2024-04-04 13:18:23 -0700

    Update createAccountList.py

--- createAccountList.py
+++ createAccountList.py
@@ -6,11 +6,21 @@ class account:
         user.first_name = first_name
         user.last_name = last_name
 
-    def get_full_name(user):
-        return f"{user.first_name} {user.last_name}"
-
+    # Verify user's email 
+    def account_info_verification()
+        
+        
+    # Verify login credentials
+    def verify_login():
+        
+    
+    # Reset user password
     def reset_password(user, new_password):
         user.password = new_password
+
+    # Returns user's name
+    def get_full_name(user):
+        return f"{user.first_name} {user.last_name}"
 
 def createAccount(username, email, password, firstname, lastname):
     user = {
