65296e236c5e2803595cccad52933a366e67af61
just-maki <165954874+just-maki@users.noreply.github.com>
2024-04-04 13:08:12 -0700

    Update createCarList.py

--- createCarList.py
+++ createCarList.py
@@ -1,3 +1,13 @@
+class Cars:
+def __init__(vin, make, model, year, price, color, type, vin, milage = None):
+    vin.make = make
+    vin.model = model
+    vin.year = year
+    vin.price = price
+    vin.color = color
+    vin.type = type
+    vin.vin = vin
+    
 car_list = []
 
 def add_user()
