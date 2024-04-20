41353d8a00a0b47ae82c92f58a6703a3469bfab8
just-maki <165954874+just-maki@users.noreply.github.com>
2024-04-19 19:08:37 -0700

    Update confirmation_email.py

--- Frontend/confirmation_email.py
+++ Frontend/confirmation_email.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for
+from flask import Flask,request,url_for
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 
