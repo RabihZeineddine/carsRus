9b5be6d1268ff51ee71f1fb553929605e3199d4b
Cristian Gomez <132181160+CrispyDAWG@users.noreply.github.com>
2024-05-02 11:33:15 -0700

    Update test_cases.py

--- Backend/test_cases.py
+++ Backend/test_cases.py
@@ -1,4 +1,8 @@
 # Pseudo Code for test cases
+# Imported files to test
+import sortingFunctions.py as sortFunc
+import createAccountList.py as accFunc
+import inputFunctions.py as inFunc
 
 # Test Sorting Functions
 
