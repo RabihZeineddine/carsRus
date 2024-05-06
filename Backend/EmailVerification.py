cb3e2081207daf1aea8c7cffa125e194445778c7
just-maki <165954874+just-maki@users.noreply.github.com>
2024-05-05 19:24:16 -0700

    Update EmailVerification.py

--- Backend/EmailVerification.py
+++ Backend/EmailVerification.py
@@ -1,20 +1,27 @@
 import smtplib
 from email.mime.text import MIMEText
 # Body of E-mail
-msg = MIMEText("This is Body of Email............")
-# Subject of E-mail
-msg['Subject'] = "This is Subject of Email........."
-# Sender E-mail Id
-sender = 'CarsRUsBot@zohomail.com'
-msg['From'] = sender
-# Receiver E-mail Id
-recipient = 'maxlebda@gmail.com'
-msg['To'] = recipient
-# SMTP Server
-server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
-# SMTP Server Login Credentials
-server.login(sender, 'pg7g p0e6 wzm7')
-# Gives Send Mail Request to SMTP Server
-server.sendmail(sender, recipient, msg.as_string())
-# Quit or Logout from SMTP Server 
-server.quit()
+def send_confirmation_email(recipient):
+    msg = MIMEText("Dear Customer,\n\nThank you for your purchase. We hope you enjoy your new ride\n\n\nBest wishes,\nThe Cars-R-Us Team")
+    # Subject of E-mail
+    msg['Subject'] = "This is Subject of Email........."
+    # Sender E-mail Id
+    sender = 'CarsRUsBot@zohomail.com'
+    msg['From'] = sender
+    # Receiver E-mail Id
+    msg['To'] = recipient
+    # SMTP Server
+    server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
+    # SMTP Server Login Credentials
+    server.login(sender, 'pg7g p0e6 wzm7')
+    # Gives Send Mail Request to SMTP Server
+    server.sendmail(sender, recipient, msg.as_string())
+    # Quit or Logout from SMTP Server
+    server.quit()
+    print("Email sent to " + recipient)
+
+def main():
+    send_confirmation_email('skoolrokz1232@gmail.com')
+
+if __name__ == "__main__":
+    main()
